85cbee4fb362e8c038a598abeeeafc7d785faee7
JoshLamptey <107364497+JoshLamptey@users.noreply.github.com>
2023-03-03 05:04:20 -1200

    Add files via upload

---
+++ Fetch/like.js
@@ -0,0 +1,9 @@
+document.getElementById("icon2").addEventListener("click",function(event){
+    let heart = event.target;
+    heart.style.color = "crimson";
+});
+
+  document.getElementById("icon3").addEventListener("click", (event) => {
+    let heart = event.target;
+    heart.style.color = "crimson";
+  });
